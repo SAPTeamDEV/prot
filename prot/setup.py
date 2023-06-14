4c4a94f96e1866b797c711dfbde57a723c507c3e
Ali-p1986 <131697486+Ali-p1986@users.noreply.github.com>
2023-06-14 22:56:15 +0330

    Add LICENSE

--- prot/setup.py
+++ prot/setup.py
@@ -1,4 +1,4 @@
-import sys, protbuilder as prot
+import sys, prot
 from setuptools import setup, find_packages
 exec(open('src/prot/__version__.py').read())
 ver = prot.VersionString(__version__)
@@ -12,7 +12,7 @@ if '!upgrade' in sys.argv:
 
 setup(
 	name="prot", version=ver, description='A Simple Tool That Contains Advance Functions.',
-	long_description=open('README.md').read(), long_description_content_type='text/markdown',
-	author="Alireza Poodineh", author_email='pa789892@gmail.com', url='https://www.pypi.org/user/Ali_p1986',
-	packages=find_packages(where="src"), package_dir={"": "src"}, zip_safe=False,
+	long_description=open('../README.md').read(), long_description_content_type='text/markdown',
+	author="Alireza Poodineh", author_email='itsaeliux@gmail.com', url='https://github.com/SAPTeamDEV/prot',
+	packages=find_packages(where="src"),license_files = ('../LICENSE',), package_dir={"": "src"}, zip_safe=False,
 	entry_points={"console_scripts": ["prot=prot:prot", "prot.pip=prot.pip:pip"]})
